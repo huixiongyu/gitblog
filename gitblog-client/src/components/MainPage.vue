62423ebeada2e3f8fb3f6c01694e948fd17d5d85
huixiongyu <huixiongyu@gmail.com>
2019-04-28 21:21:32 +0800

    MenuItem activeName be saved to localstorage

--- gitblog-client/src/components/MainPage.vue
+++ gitblog-client/src/components/MainPage.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="main-content">
-        <Menu active-name="overview" mode="horizontal" style="width: 100%">
+        <Menu :active-name="activeName" 
+                @on-select="selectItem"
+                mode="horizontal" 
+                style="width: 100%">
             <MenuItem name="overview" to="/">Overview</MenuItem>
             <MenuItem name="posts" to="/posts">Posts</MenuItem>
             <MenuItem name="categories" to="/categories">Categories</MenuItem>
@@ -15,6 +18,23 @@
 </template>
 <script>
 export default {
+    data(){
+        return {
+            activeName: 'overview'
+        }
+    },
+    methods: {
+        selectItem(name){
+            localStorage.setItem('currentTab', name);
+            // console.log(name);
+        }
+    },
+    created(){
+        const name = localStorage.getItem('currentTab');
+        if(name){
+            this.activeName = name;
+        }
+    }
 }
 </script>
 <style lang="less">
